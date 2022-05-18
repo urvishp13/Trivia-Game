86dfff9cae07651f97f2edadcb0424ded2f48c96
Picasso <urvishp13@gmail.com>
2022-05-17 22:30:44 -0400

    extracted answers submitted by uses

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,22 @@
+// Add a 120s timer
+
 // Create a questions, question's answer choices, question's answer storage
 var storage = [
     {
+        questionNumber: "question1",
         question: "What is the 2nd planet from the Sun?",
         answerChoices: [
-            "Mars", "Venus", "Earth", "Mecury"
+            "Mars", "Venus", "Earth", "Mercury"
         ],
         answer: "Venus"
+    },
+    {
+        questionNumber: "question2",
+        question: "What is the 4th planet from the Sun?",
+        answerChoices: [
+            "Mars", "Venus", "Earth", "Mercury"
+        ],
+        answer: "Mars"
     }
 ];
 
@@ -13,50 +24,80 @@ var storage = [
 var questionsSection = document.getElementById("questions-section");
 // write questions and question answer choices to DOM
 storage.forEach(questionSet => {
-    let oneQuestionSection = document.createElement("div");
+    let oneQuestion = document.createElement("div");
 
     // Question
     let question = document.createElement("p");
+    question.setAttribute("class", "question");
     let questionText = document.createTextNode(`${questionSet.question}`);
     question.appendChild(questionText);
 
     // Add question to questionsSection
-    oneQuestionSection.appendChild(question);
+    oneQuestion.appendChild(question);
 
     // Answer choices for this question
-    let answerChoicesSection = document.createElement("div");
+    let answerChoices = document.createElement("div");
+    answerChoices.setAttribute("class", "question-answer-choices");
+
     questionSet.answerChoices.forEach(answerChoice => {
         // Section for one answer choice
-        let oneAnswerChoiceSection = document.createElement("div");
+        let oneAnswerChoice = document.createElement("div");
 
         // Create radio nutton for answer choice
         let oneAnswerChoiceRadio = document.createElement("input");
         oneAnswerChoiceRadio.setAttribute("type", "radio");
-        oneAnswerChoiceRadio.setAttribute("id", `${answerChoice}`);
+        oneAnswerChoiceRadio.setAttribute("id", `${questionSet.questionNumber}.${answerChoice}`);
         oneAnswerChoiceRadio.setAttribute("name", `${questionSet.question}`);
         oneAnswerChoiceRadio.setAttribute("value", `${answerChoice}`);
 
         // Add text to this radio button
         let oneAnswerChoiceLabel = document.createElement("label");
-        oneAnswerChoiceLabel.setAttribute("for", `${answerChoice}`);
+        oneAnswerChoiceLabel.setAttribute("for", `${questionSet.questionNumber}.${answerChoice}`);
         let labelText = document.createTextNode(`${answerChoice}`);
         oneAnswerChoiceLabel.appendChild(labelText);
 
         // Append all data for this answer choice to oneAnswerChoice section
-        oneAnswerChoiceSection.appendChild(oneAnswerChoiceRadio);
-        oneAnswerChoiceSection.appendChild(oneAnswerChoiceLabel);
+        oneAnswerChoice.appendChild(oneAnswerChoiceRadio);
+        oneAnswerChoice.appendChild(oneAnswerChoiceLabel);
 
         // Add this answer choice to the entire answerCHoicesSection for his question
-        answerChoicesSection.appendChild(oneAnswerChoiceSection);
+        answerChoices.appendChild(oneAnswerChoice);
         //console.log("appenddd: " + `${answerChoice}`);
     });
 
     // Add all the answer choices for the quesetion to the DOM
-    oneQuestionSection.appendChild(answerChoicesSection);
+    oneQuestion.appendChild(answerChoices);
 
     // Add the one question to the entire questionsSection
-    questionsSection.appendChild(oneQuestionSection);
+    questionsSection.appendChild(oneQuestion);
 });
 
-// When Trivia submitted
-    // Figure out which questions are right, wrong, and unanswered and overall score
+function results() {
+    // Figure out which questions are right, wrong, and unanswered and overall score, and write them to DOM
+    //console.log(Object.values($('.question-answer-choices')));
+    let answersSubmitted = [];
+    let answerChoicesSets = Object.values($('.question-answer-choices'));
+    for (let i = 0; i < answerChoicesSets.length; i++) {
+        var answerChoicesSet = answerChoicesSets[i];
+        
+        // If went through all the answer choice sets
+        if (answerChoicesSet == storage.length) {
+            break;
+        }
+        
+        // If question unanswered
+        if ($(answerChoicesSet).find('input[type="radio"]:checked').length != 1) {
+            answersSubmitted.push(null);
+        }
+        else {
+            answersSubmitted.push($(answerChoicesSet).find('input[type="radio"]:checked').val());
+        }
+    }
+
+    return answersSubmitted;
+}
+
+// When Trivia submitted or timer runs out
+$('#submit').on('click', () => {
+    let answersSubmitted = results();
+});
